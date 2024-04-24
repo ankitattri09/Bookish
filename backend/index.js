8eb6734ec28a715bb0f69b7bf8c687c5b8c7c4a6
aa09 <ankitattri.pvt3@gmail.com>
2024-04-24 10:26:00 +0530

    deploy

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const  dotenv=require("dotenv");
+const app = express();
+
+dotenv.config();
+const PORT=process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('bookish');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
